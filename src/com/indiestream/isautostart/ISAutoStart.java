9397230f63d75e889e961e532aae5f632eb4e166
IndieStream <couture.peter@3rdquark.com>
2017-02-18 15:08:45 -0800

    Updated comments and adding example .xml files

--- src/com/indiestream/isautostart/ISAutoStart.java
+++ src/com/indiestream/isautostart/ISAutoStart.java
@@ -1,43 +1,66 @@
+/*
+* This code and all components Copyright (c) 2017 IndieStream, LLC. All rights reserved.
+* This code is licensed pursuant to the agreement in the LICENSE file.
+*/
+
 package com.indiestream.isautostart;
 
-import com.wowza.wms.application.*;
-import com.wowza.wms.amf.*;
-import com.wowza.wms.client.*;
-import com.wowza.wms.module.*;
-import com.wowza.wms.request.*;
-import com.wowza.wms.stream.*;
+import com.wowza.wms.amf.AMFDataList;
+import com.wowza.wms.application.IApplicationInstance;
+import com.wowza.wms.client.IClient;
+import com.wowza.wms.module.ModuleBase;
+import com.wowza.wms.request.RequestFunction;
 import com.wowza.wms.stream.publish.Playlist;
 import com.wowza.wms.stream.publish.Stream;
-import com.wowza.wms.rtp.model.*;
-import com.wowza.wms.httpstreamer.model.*;
-import com.wowza.wms.httpstreamer.cupertinostreaming.httpstreamer.*;
-import com.wowza.wms.httpstreamer.smoothstreaming.httpstreamer.*;
 
+/**
+ * 
+ * ISAutoStart - This is a Wowza module that will start streaming a 
+ * VOD file live. 
+ * 
+ * Find  https://github.com/indiestream/ISAutoStart
+ * 
+ * The following properties must be set in the application's configuration file.
+ *  
+ * autoStartFileName - The name of the file to be streamed. Loacted in the content/ folder by default.
+ * autoStartStreamName - The name of the output stream.
+ * autoStartStartTime - The start time to begin streaming at.
+ * autoStartDuration -  The duration the file will be played till.
+ *  
+ * @author petercouture
+ *
+ */
 public class ISAutoStart extends ModuleBase {
 	
 	private Playlist _playlist;
 	private Stream _stream;
-	private String _streamName = "feed";
 	
 	public void onAppStart(IApplicationInstance appInstance) {
+		
 		String fullname = appInstance.getApplication().getName() + "/" + appInstance.getName();
 		getLogger().info("onAppStart: " + fullname);
-		
 
-		// Create the playlist the cameras will cycle through
+		// Create the playlist that will contain the VOD item to be streamed out.
 		_playlist = new Playlist("playlist");
 		
 		// Set the playlist so it repeats when it gets to the end of the list.
 		_playlist.setRepeat(true);
 		
-		// Add the default stream which is a VOD file stored in the /content folder.
-		// This file will play when no cameras are publishing to the app.
-		_playlist.addItem("mp4:3_cloning.mp4", 0, 3600);
-
-		// Create the stream that will be published out
-		_stream = Stream.createInstance(appInstance, _streamName);
+		// Pull the configuration properties from the Application.xml
+		String fileName = appInstance.getProperties().getPropertyStr("autoStartFileName");
+		String streamName = appInstance.getProperties().getPropertyStr("autoStartStreamName");
+		int startTime = appInstance.getProperties().getPropertyInt("autoStartStartTime", 0);
+		int duration = appInstance.getProperties().getPropertyInt("autoStartDuration", 10);
 		
-		// Open the stream using the content in the playlist. To start will be the 'Offline' video.
+		getLogger().info("onAppStart: ISAutoStart: Autostarting file : " + fileName + " at " + startTime + " till " + duration + " on stream: " + streamName);
+		
+		// Add the VOD file to the playlist which will be streamed out.
+		_playlist.addItem(fileName, startTime, duration);
+
+		// Create the stream that will be published out.
+		_stream = Stream.createInstance(appInstance, streamName);
+		
+		// Start the live stream.
 		_playlist.open(_stream);
 		
 	}
--- src/com/indiestream/isautostart/ISAutoStartServer.java
+++ src/com/indiestream/isautostart/ISAutoStartServer.java
@@ -1,3 +1,8 @@
+/*
+* This code and all components Copyright (c) 2017 IndieStream, LLC. All rights reserved.
+* This code is licensed pursuant to the agreement in the LICENSE file.
+*/
+
 package com.indiestream.isautostart;
 
 import com.wowza.wms.logging.WMSLoggerFactory;
@@ -5,50 +10,38 @@ import com.wowza.wms.server.IServer;
 import com.wowza.wms.server.IServerNotify2;
 import com.wowza.wms.vhost.VHostSingleton;
 
-
 /**
  * 
  * ISAutoStartServer - This is the server listener class. It 
  * tells the VHost to start the application once the server 
  * has initialized.
  * 
- * You need to add it to the ServerListeners node at the 
- * bottom of /conf/server.xml
+ * autoStartVHostName and autoStartApplicationName must be set in the servers conf/Server.xml
+ * file. 
  *   
  * @author petercouture
  *
  */
 public class ISAutoStartServer implements IServerNotify2 {
 
-	// The listener for the VHost events.
-	ISAutoStartVHost listenerVHost;
 	
 	public void onServerConfigLoaded(IServer server) {
 		WMSLoggerFactory.getLogger(null).info("onServerConfigLoaded");
 	}
 
 	public void onServerCreate(IServer server) {
-		
 		WMSLoggerFactory.getLogger(null).info("onServerCreate");
-
-		// Create a new event listener for the default VHost.
-		//listenerVHost = new ISAutoStartVHost();
-		//VHostSingleton.addVHostListener(listenerVHost);
 	}
 
 	public void onServerInit(IServer server) {
 		WMSLoggerFactory.getLogger(null).info("onServerInit");
+		
 		// Once the server has initialized, tell the default VHost to start the application.
-		//listenerVHost.startApplication();
 		VHostSingleton.getInstance(server.getProperties().getPropertyStr("autoStartVHostName")).startApplicationInstance(server.getProperties().getPropertyStr("autoStartApplicationName"));
 	}
 
 	public void onServerShutdownStart(IServer server) {
 		WMSLoggerFactory.getLogger(null).info("onServerShutdownStart");
-		
-		// Remove the listener and set it to null.
-		//VHostSingleton.removeApplicationListener(listenerVHost);
-		//listenerVHost = null;
 	}
 
 	public void onServerShutdownComplete(IServer server) {
